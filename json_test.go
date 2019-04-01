92cc930e7242b7b332cfb926e1a1674c449f59da
Tim Kluge <timklge@gmail.com>
2019-04-01 18:42:50 +0200

    Add null to json test

--- json_test.go
+++ json_test.go
@@ -3,8 +3,33 @@ package packrat
 import "testing"
 
 func TestJSON(t *testing.T) {
-	input := `{"hallo": "3", "welt": true, "subObject": {"hallo": 5}, "subArray": [1,2,3,4,5,6,7],
-		"tek": "lel test \" halolaleö pops"}`
+	input := `{"menu": {
+		"header": "SVG Viewer",
+		"items": [
+			{"id": "Open"},
+			{"id": "OpenNew", "label": "Open New"},
+			null,
+			{"id": "ZoomIn", "label": "Zoom In"},
+			{"id": "ZoomOut", "label": "Zoom Out"},
+			{"id": "OriginalView", "label": "Original View"},
+			null,
+			{"id": "Quality"},
+			{"id": "Pause"},
+			{"id": "Mute"},
+			null,
+			{"id": "Find", "label": "Find..."},
+			{"id": "FindAgain", "label": "Find Again"},
+			{"id": "Copy"},
+			{"id": "CopyAgain", "label": "Copy Again"},
+			{"id": "CopySVG", "label": "Copy SVG"},
+			{"id": "ViewSVG", "label": "View SVG"},
+			{"id": "ViewSource", "label": "View Source"},
+			{"id": "SaveAs", "label": "Save As"},
+			null,
+			{"id": "Help"},
+			{"id": "About", "label": "About Adobe CVG Viewer..."}
+		]
+	}}`
 	scanner := NewScanner(input, true)
 
 	stringParser := NewAndParser(NewAtomParser(`"`, true), NewRegexParser(`(?:[^"\\]|\\.)*`, false), NewAtomParser(`"`, false))
@@ -12,10 +37,11 @@ func TestJSON(t *testing.T) {
 	propParser := NewAndParser(stringParser, NewAtomParser(":", true), valueParser)
 
 	objParser := NewAndParser(NewAtomParser("{", true), NewKleeneParser(propParser, NewAtomParser(",", true)), NewAtomParser("}", true))
+	nullParser := NewAtomParser("null", true)
 	numParser := NewRegexParser(`-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?`, true)
 	boolParser := NewRegexParser("(true|false)", true)
 	arrayParser := NewAndParser(NewAtomParser("[", true), NewKleeneParser(valueParser, NewAtomParser(",", true)), NewAtomParser("]", true))
-	valueParser.Set(objParser, stringParser, numParser, boolParser, arrayParser)
+	valueParser.Set(nullParser, objParser, stringParser, numParser, boolParser, arrayParser)
 
 	n, err := Parse(valueParser, scanner)
 	if err != nil {
